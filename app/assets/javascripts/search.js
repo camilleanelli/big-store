d3b41bb75bc691388308e5e74aaa2591afbf84fd
camille anelli <camilleanelli32@gmail.com>
2017-04-10 17:27:03 +0200

    fix bug browser

--- app/assets/javascripts/search.js
+++
@@ -1,6 +0,0 @@
-function triggerValueSearch(helper) {
-  $('#search-box').on('keyup', function() {
-    var currentSearchValue = $(this).val();
-    helper.setQuery(currentSearchValue).search();
-  });
-}
